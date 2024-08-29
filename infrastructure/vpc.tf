46d51cbccd5f9f83a7355e85a1e2c5f7f9ebcd49
Mauro Baraldi <mauro.baraldi@gmail.com>
2024-08-29 14:20:07 +0100

    Adding Internet Gateway resources.

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -39,3 +39,54 @@ resource "aws_subnet" "database_subnets" {
     Name = "rr-data-subnet${count.index + 1}-${element(var.azs, count.index)}"
   }
 }
+
+# Internet Gateway Resources
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "rr-igw"
+  }
+
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_subnets,
+  ]
+}
+
+resource "aws_route_table" "route_table_igw_pub" {
+  vpc_id = aws_vpc.main.id
+
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.internet_gateway.id
+  # }
+
+  tags = {
+    Name = "rr-route-table-igw-pub"
+  }
+
+  depends_on = [
+    aws_internet_gateway.internet_gateway
+  ]
+}
+
+resource "aws_route" "public_internet_igw_route" {
+  route_table_id         = aws_route_table.route_table_igw_pub.id
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+
+  depends_on = [
+    aws_route_table.route_table_igw_pub
+  ]
+}
+
+resource "aws_route_table_association" "route_igw_pub" {
+  count          = length(var.application_subnet_cidrs)
+  route_table_id = aws_route_table.route_table_igw_pub.id
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+
+  depends_on = [
+    aws_route.public_internet_igw_route
+  ]
+}
