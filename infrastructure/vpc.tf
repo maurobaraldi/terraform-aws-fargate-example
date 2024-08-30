32acdba2e4cc69b93624da553848d77a126599a1
Mauro Baraldi <mauro.baraldi@gmail.com>
2024-08-30 10:18:38 +0100

    Adding NAT Gateway resources.

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -90,3 +90,88 @@ resource "aws_route_table_association" "route_igw_pub" {
     aws_route.public_internet_igw_route
   ]
 }
+
+# NAT Gateway Resources
+# You must define the Elastic IP in NAT Gateway, so define it first
+resource "aws_eip" "elastic_ip_nat_gateway_a" {
+  domain   = "vpc"
+
+  tags     = {
+    Name = "rr-eip-ngw-pub-a"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway_pub_a" {
+  subnet_id     = aws_subnet.public_subnets[0].id
+  allocation_id = aws_eip.elastic_ip_nat_gateway_a.id
+
+  tags          = {
+    Name = "rr-ngw-pub-a"
+  }
+
+  depends_on    = [
+    aws_internet_gateway.internet_gateway
+  ]
+}
+
+resource "aws_eip" "elastic_ip_nat_gateway_b" {
+  domain   = "vpc"
+  tags     = {
+    Name = "rr-eip-ngw-pub-b"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway_pub_b" {
+  subnet_id     = aws_subnet.public_subnets[1].id
+  allocation_id = aws_eip.elastic_ip_nat_gateway_b.id
+
+  tags          = {
+    Name = "rr-ngw-pub-b"
+  }
+
+  depends_on    = [
+    aws_internet_gateway.internet_gateway
+  ]
+}
+
+# Routing table for private subnet in Availability - Zone A
+resource "aws_route_table" "route_table_private_a" {
+  vpc_id = aws_vpc.main.id
+  tags   = {
+    Name = "rr-rt-priv-a"
+  }
+}
+
+# Route Access to the Internet through NAT - Zone A
+resource "aws_route" "route_app_sn_a_ngw_a" {
+  route_table_id         = aws_route_table.route_table_private_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat_gateway_pub_a.id
+}
+
+# Routing Table Association for Subnet application_subnets - Zone A
+resource "aws_route_table_association" "route_app_sn_ngw_a" {
+  subnet_id      = aws_subnet.application_subnets[0].id
+  route_table_id = aws_route_table.route_table_private_a.id
+}
+
+# Routing table for private subnet in Availability - Zone B
+resource "aws_route_table" "route_table_private_b" {
+  vpc_id = aws_vpc.main.id
+  tags   = {
+    Name = "rr-rt-priv-b"
+  }
+}
+
+# Route Access to the Internet through NAT - Zone B
+resource "aws_route" "route_app_sn_a_ngw_b" {
+  route_table_id         = aws_route_table.route_table_private_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat_gateway_pub_b.id
+}
+
+# Routing Table Association for Subnet application_subnets - Zone B
+resource "aws_route_table_association" "route_app_sn_ngw_b" {
+  subnet_id      = aws_subnet.application_subnets[1].id
+  route_table_id = aws_route_table.route_table_private_b.id
+}
